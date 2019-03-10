7ca7a50cfec59750dda8a867f5476f8b18198f47
Joe Maffei <joemaffei@users.noreply.github.com>
2019-03-10 11:35:12 -0500

    tabs -> spaces

--- src/bemDefinitions.js
+++ src/bemDefinitions.js
@@ -2,21 +2,20 @@ import isFunction from 'lodash/isFunction';
 import getStyledComponentId from './getStyledComponentId';
 
 const bemDefinitions = modifiers => props => {
-
-	const id = getStyledComponentId(props);
-	if (!id) return '';
-
-	const definitions = Object.keys(modifiers).reduce(
+  const id = getStyledComponentId(props);
+  if (!id) return '';
+  
+  const definitions = Object.keys(modifiers).reduce(
     (result, mod) =>
-      `${result}&.${id}-${mod}{${
-				isFunction(modifiers[mod])
-					? modifiers[mod](props)
-					: modifiers[mod]
-      }}`,
+    `${result}&.${id}-${mod}{${
+      isFunction(modifiers[mod])
+        ? modifiers[mod](props)
+        : modifiers[mod]
+    }}`,
     ''
   );
 
-	return definitions;
+  return definitions;
 };
 
 export default bemDefinitions;
