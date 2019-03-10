4bf4512415e3beaec060ffd3965b5a0ab5782dcd
Joe Maffei <joemaffei@users.noreply.github.com>
2019-03-10 12:26:24 -0500

    removed lodash

--- src/bemDefinitions.js
+++ src/bemDefinitions.js
@@ -1,14 +1,13 @@
-import isFunction from 'lodash/isFunction';
 import getStyledComponentId from './getStyledComponentId';
 
 const bemDefinitions = modifiers => props => {
   const id = getStyledComponentId(props);
   if (!id) return '';
-  
+
   const definitions = Object.keys(modifiers).reduce(
     (result, mod) =>
     `${result}&.${id}-${mod}{${
-      isFunction(modifiers[mod])
+      typeof modifiers[mod] === 'function'
         ? modifiers[mod](props)
         : modifiers[mod]
     }}`,
