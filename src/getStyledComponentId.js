8f00491136c866288c51e5a3402f4cc34ac55422
Joe Maffei <joemaffei@users.noreply.github.com>
2019-03-10 12:40:51 -0500

    moved everything to index.js

--- src/getStyledComponentId.js
+++
@@ -1,8 +0,0 @@
-const getStyledComponentId = (componentOrProps = {}) => {
-  let id = componentOrProps.styledComponentId;
-  if (!id) id = (componentOrProps.forwardedComponent || {}).styledComponentId;
-  if (!id) return;
-  return id.replace(/^sc\-/, '');
-}
-
-export default getStyledComponentId;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
-import bemClassNames from './bemClassNames';
-import bemDefinitions from './bemDefinitions';
-import getStyledComponentId from './getStyledComponentId';
+import classnames from 'classnames';
 
-const styledComponentsBem = {
-  bemClassNames,
-  bemDefinitions,
-  getStyledComponentId
+export const getStyledComponentId = (componentOrProps = {}) => {
+  let id = componentOrProps.styledComponentId;
+  if (!id) id = (componentOrProps.forwardedComponent || {}).styledComponentId;
+  if (!id) return;
+  return id.replace(/^sc-/, '');
+}
+
+export const bemClassNames = modifiers => componentOrProps => {
+  const id = getStyledComponentId(componentOrProps);
+  if (!id) return '';
+
+  const classObject = Object.keys(modifiers).reduce(
+    (result, mod) => ({
+      ...result,
+      [`${id}-${mod}`]: componentOrProps[mod]
+    }),
+    {}
+  );
+
+  return classnames(classObject);
 };
 
-export default styledComponentsBem;
+export const bemDefinitions = modifiers => props => {
+  const id = getStyledComponentId(props);
+  if (!id) return '';
+
+  const definitions = Object.keys(modifiers).reduce(
+    (result, mod) =>
+    `${result}&.${id}-${mod}{${
+      typeof modifiers[mod] === 'function'
+        ? modifiers[mod](props)
+        : modifiers[mod]
+    }}`,
+    ''
+  );
+
+  return definitions;
+};
