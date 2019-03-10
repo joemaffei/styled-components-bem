a027eab560cfb8a40a0afbbb76e63d5112a55b01
Joe Maffei <joemaffei@users.noreply.github.com>
2019-03-10 12:52:01 -0500

    updated packages

--- src/test/getStyledComponentId.test.js
+++ src/test/getStyledComponentId.test.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import getStyledComponentId from '../getStyledComponentId';
+import { getStyledComponentId } from '../index';
 
 describe('getStyledComponentId', () => {
   const mockProps = {
